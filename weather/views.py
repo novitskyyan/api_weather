9617ad099307c6c087935dabaaf73447c2985a47
Стас novitskyyan² <novitskyyan@gmail.com>
2024-02-28 20:34:33 +0300

    V4.

--- weather/views.py
+++ weather/views.py
@@ -8,17 +8,21 @@ from weather_api import weather
 
 def index(request):
     sleep(2)
+    # TODO: В случае если json Не пустой, его содержимое передать в context и
+    # TODO: и Применить render. В обратном случае создать словарь с правильными
+    # TODO: ключами и структурой и записать его в json.
     with open('weather.json', "r", encoding="UTF-8") as file:
         if len(file.read()) != 0:
             file.seek(0)
             context = loads(file.read())
         else:
-            context = {"1": {"city": "ыпквпв", "weather": []},
-                       "2": {"city": "пвпвпв", "weather": []},
-                       "3": {"city": "впвпав", "weather": []},
-                       "4": {"city": "впвпввп", "weather": []}}
+            # TODO: Правильно выбрать формат словаря, чтобы можно было подставить в шаблон. Плюс понять, как можно подставлять словаря в шаблон django.
+            context = {"1": {"city": None, "weather": None},
+                       "2": {"city": None, "weather": None},
+                       "3": {"city": None, "weather": None},
+                       "4": {"city": None, "weather": None}}
             with open("weather.json", "w", encoding='UTF-8') as json_file:
-                dump(context, json_file)
+                dump(context, json_file, ensure_ascii=False)
         print(context)
     return render(request, 'weather/index.html', context)
 
@@ -27,8 +31,15 @@ def get_weather(request):
     if request.method == 'GET':
         city = request.GET.get("city")
         w = weather.Weather(city)
-        info = {'city': city, 'weather': w.get_info()[1:]}
+
+        with open("weather.json", "r", encoding='UTF-8') as json_file:
+            context = loads(json_file.read())
+            if context["1"]['city'] is None:
+                context['1']['city'] = city
+                context['1']['weather'] = w.get_info()[1:]
+
+        # TODO: Обрабатываем json: 1. считываем json; если есть свободное место из 4, то добавляем город и информацию по погоде; в другом случае, если там есть 4 города с информацией, то удаляем последний 4 и кладем новый в начало.
         with open("weather.json", "w", encoding='UTF-8') as json_file:
-            dump(info, json_file)
+            dump(context, json_file, ensure_ascii=False)
         data = {"message": "ok"}
         return JsonResponse(data)
